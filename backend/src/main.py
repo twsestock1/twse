52de965a967f2b98defda489c76b8bf9d5111ed8
Wang <twsestock1@gmail.com>
2026-07-01 22:51:44 +0800

    modify docker-compose & .gitignore

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
-def hello():
-    return {"msg": "hello"}
+def say_hello(name: str):
+
+    if name.strip() == "":
+        return {
+            "error": "名字不能為空"
+        }
+
+    return {
+        "message": f"Hello, {name}"
+    }
